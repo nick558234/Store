70f99e32b8cd325f8ab1e7d0416fc31a5ef69d27
nickvanhooff <nickvanhooff03@gmail.com>
2024-09-06 10:03:46 +0200

    Add initial project files and configurations

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Winkelvooraadbeheer</h1>
+        <nav>
+    <NuxtLink :to="'/products'">Producten toevoegen</NuxtLink>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: 'Winkelvooraadbeheer'
+})
+
+
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Producten beheer</h1>
+        <div>
+            <label for="name">Name</label>
+            <input id="name" placeholder="Naam" v-model="productData.name" />
+            <button @click="addProduct()">Product toevoegen</button>
+        </div>
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                {{ productData }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+useHead({
+    title: 'Producten beheer'
+});
+
+const productData = ref({ name: '' });
+const products = ref([]);
+
+const addProduct = () => {
+    products.value.push({
+        id: products.value.length + 1,
+        name: productData.name
+    });
+};
+</script>
