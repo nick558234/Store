d2e04f65279c096da4fdf1ee7a73ba04b71ae55b
nickvanhooff <nickvanhooff03@gmail.com>
2024-11-01 15:39:43 +0100

    refactor Header component to remove unnecessary background class

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-gray-200 p-4 flex justify-between items-center">
+    <header class="p-4 flex justify-between items-center">
         <h1 class="text-3xl font-bold">Winkelvooraadbeheer</h1>
       </header>
 </template>
